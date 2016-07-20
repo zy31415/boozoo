81b5ae5d760ed5a1a8a38224d26b46dd160bbbb9
Yang Zhang <zy31415@gmail.com>
2016-07-19 20:23:11 -0700

    -mAdd a dependency.

--- src/main/java/boozoo/App.java
+++ src/main/java/boozoo/App.java
@@ -1,5 +1,6 @@
 package boozoo;
 
+import java.sql.*;
 /**
  * Hello world!
  *
@@ -8,6 +9,16 @@ public class App
 {
     public static void main( String[] args )
     {
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+
         System.out.println( "Hello World!" );
     }
 }
