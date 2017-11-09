d9cb2273811aca7d63af6b04f27fabaacb71cb34
Yang Zhang <zy31415@gmail.com>
2017-11-08 20:54:56 -0800

    Add logging system.

--- src/main/java/io/github/zy31415/boozoo/database/BoozooEMF.java
+++ src/main/java/io/github/zy31415/boozoo/database/BoozooEMF.java
@@ -1,8 +1,8 @@
 package io.github.zy31415.boozoo.database;
 
 import com.google.common.collect.ImmutableMap;
-import com.sun.media.jfxmedia.logging.Logger;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.nio.file.Path;
@@ -16,22 +16,28 @@ public class BoozooEMF {
 
     private BoozooEMF() {}
 
+    final static Logger logger = LogManager.getLogger();
+
     private static final String BOOZOOROOT = getBOOZOOROOT();
     private static final String DbUrl = getDbUrl();
 
     private static String getBOOZOOROOT() {
-        String envBOOZOOROOT = System.getenv("BOOZOOROOT");
+        String booZooRoot = System.getenv("BOOZOOROOT");
 
-        if (null == envBOOZOOROOT) {
-            return "~/.boozoo/";
-        } else {
-            return envBOOZOOROOT;
+        if (null == booZooRoot) {
+            booZooRoot = "~/.boozoo/";
         }
+
+        logger.info("BOOZOOROOT is {}", booZooRoot);
+        return booZooRoot;
+
     }
 
     private static String getDbUrl() {
         Path dbPath = Paths.get(BOOZOOROOT, "db", "boozoo.h2");
-        return String.format("jdbc:h2:%s", dbPath.toString());
+        String dbUrl = String.format("jdbc:h2:%s", dbPath.toString());
+        logger.info("Database URL is {}", dbUrl);
+        return dbUrl;
     }
 
     private static EntityManagerFactory emf = Persistence.createEntityManagerFactory(
